e83b4cf9ab8c660110f5fbc1f9848e49489efa71
vandosant <sjskender@gmail.com>
2016-05-29 23:04:19 -0600

    Create reverser

---
+++ Reverse.elm
@@ -0,0 +1,46 @@
+import Html exposing (Html, Attribute, div, input, text, button)
+import Html.App as Html
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput, onClick)
+import String
+
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+-- MODEL
+
+
+type alias Model =
+  { content: String
+  }
+
+model : Model
+model =
+  { content = "" }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String | Clear
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent }
+    Clear ->
+      { model | content = "" }
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "Text to reverse", onInput Change ] []
+    , div [] [ text (String.reverse model.content) ]
+    , button [ onClick Clear ] [ text "CLEAR" ]
+    ]
